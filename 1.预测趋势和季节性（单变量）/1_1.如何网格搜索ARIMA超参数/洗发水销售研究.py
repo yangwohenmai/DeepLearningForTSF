1f2c625acc719a1af4c7968fae9c49b3c8856d54
yangwohenmai <zongxianyang@outlook.com>
2020-07-07 13:49:21 +0800

    update

---
+++ 1.预测趋势和季节性（单变量）/1_1.如何网格搜索ARIMA超参数/洗发水销售研究.py
@@ -0,0 +1,51 @@
+import warnings
+from pandas import read_csv
+from pandas import datetime
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+
+# 评估给定订单的ARIMA模型（p，d，q）
+def evaluate_arima_model(X, arima_order):
+	# prepare training dataset
+	train_size = int(len(X) * 0.66)
+	train, test = X[0:train_size], X[train_size:]
+	history = [x for x in train]
+	# 进行预测
+	predictions = list()
+	for t in range(len(test)):
+		model = ARIMA(history, order=arima_order)
+		model_fit = model.fit(disp=0)
+		yhat = model_fit.forecast()[0]
+		predictions.append(yhat)
+		history.append(test[t])
+	# 计算样本的误差值
+	error = mean_squared_error(test, predictions)
+	return error
+
+# 评估ARIMA模型的p，d和q值的组合
+def evaluate_models(dataset, p_values, d_values, q_values):
+	dataset = dataset.astype('float32')
+	best_score, best_cfg = float("inf"), None
+	for p in p_values:
+		for d in d_values:
+			for q in q_values:
+				order = (p,d,q)
+				try:
+					mse = evaluate_arima_model(dataset, order)
+					if mse < best_score:
+						best_score, best_cfg = mse, order
+					print('ARIMA%s MSE=%.3f' % (order,mse))
+				except:
+					continue
+	print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
+
+# load dataset
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# 评估参数
+p_values = [0, 1, 2, 4, 6, 8, 10]
+d_values = range(0, 3)
+q_values = range(0, 3)
+warnings.filterwarnings("ignore")
+evaluate_models(series.values, p_values, d_values, q_values)
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # DeepLearning4TimeSeriesForecasting
 深度学习以进行时间序列预测
 ## 序言  
-7天迷你课
+7天迷你课 
++1
+-2
+## 预测趋势和季节性（单变量）
