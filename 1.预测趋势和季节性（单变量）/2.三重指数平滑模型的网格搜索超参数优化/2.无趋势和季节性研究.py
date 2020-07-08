10716ff88ee26ca00c4bc731b27f9bdd571d77ec
yangwohenmai <zongxianyang@outlook.com>
2020-07-08 10:15:36 +0800

    update

---
+++ 1.预测趋势和季节性（单变量）/2.三重指数平滑模型的网格搜索超参数优化/2.无趋势和季节性研究.py
@@ -0,0 +1,124 @@
+# grid search ets models for daily female births
+from math import sqrt
+from multiprocessing import cpu_count
+from joblib import Parallel
+from joblib import delayed
+from warnings import catch_warnings
+from warnings import filterwarnings
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from sklearn.metrics import mean_squared_error
+from pandas import read_csv
+from numpy import array
+
+# one-step Holt Winter¡¯s Exponential Smoothing forecast
+def exp_smoothing_forecast(history, config):
+	t,d,s,p,b,r = config
+	# define model
+	history = array(history)
+	model = ExponentialSmoothing(history, trend=t, damped=d, seasonal=s, seasonal_periods=p)
+	# fit model
+	model_fit = model.fit(optimized=True, use_boxcox=b, remove_bias=r)
+	# make one step forecast
+	yhat = model_fit.predict(len(history), len(history))
+	return yhat[0]
+
+# root mean squared error or rmse
+def measure_rmse(actual, predicted):
+	return sqrt(mean_squared_error(actual, predicted))
+
+# split a univariate dataset into train/test sets
+def train_test_split(data, n_test):
+	return data[:-n_test], data[-n_test:]
+
+# walk-forward validation for univariate data
+def walk_forward_validation(data, n_test, cfg):
+	predictions = list()
+	# split dataset
+	train, test = train_test_split(data, n_test)
+	# seed history with training dataset
+	history = [x for x in train]
+	# step over each time-step in the test set
+	for i in range(len(test)):
+		# fit model and make forecast for history
+		yhat = exp_smoothing_forecast(history, cfg)
+		# store forecast in list of predictions
+		predictions.append(yhat)
+		# add actual observation to history for the next loop
+		history.append(test[i])
+	# estimate prediction error
+	error = measure_rmse(test, predictions)
+	return error
+
+# score a model, return None on failure
+def score_model(data, n_test, cfg, debug=False):
+	result = None
+	# convert config to a key
+	key = str(cfg)
+	# show all warnings and fail on exception if debugging
+	if debug:
+		result = walk_forward_validation(data, n_test, cfg)
+	else:
+		# one failure during model validation suggests an unstable config
+		try:
+			# never show warnings when grid searching, too noisy
+			with catch_warnings():
+				filterwarnings("ignore")
+				result = walk_forward_validation(data, n_test, cfg)
+		except:
+			error = None
+	# check for an interesting result
+	if result is not None:
+		print(' > Model[%s] %.3f' % (key, result))
+	return (key, result)
+
+# grid search configs
+def grid_search(data, cfg_list, n_test, parallel=True):
+	scores = None
+	if parallel:
+		# execute configs in parallel
+		executor = Parallel(n_jobs=cpu_count(), backend='multiprocessing')
+		tasks = (delayed(score_model)(data, n_test, cfg) for cfg in cfg_list)
+		scores = executor(tasks)
+	else:
+		scores = [score_model(data, n_test, cfg) for cfg in cfg_list]
+	# remove empty results
+	scores = [r for r in scores if r[1] != None]
+	# sort configs by error, asc
+	scores.sort(key=lambda tup: tup[1])
+	return scores
+
+# create a set of exponential smoothing configs to try
+def exp_smoothing_configs(seasonal=[None]):
+	models = list()
+	# define config lists
+	t_params = ['add', 'mul', None]
+	d_params = [True, False]
+	s_params = ['add', 'mul', None]
+	p_params = seasonal
+	b_params = [True, False]
+	r_params = [True, False]
+	# create config instances
+	for t in t_params:
+		for d in d_params:
+			for s in s_params:
+				for p in p_params:
+					for b in b_params:
+						for r in r_params:
+							cfg = [t,d,s,p,b,r]
+							models.append(cfg)
+	return models
+
+if __name__ == '__main__':
+	# load dataset
+	series = read_csv('daily-total-female-births.csv', header=0, index_col=0)
+	data = series.values
+	# data split
+	n_test = 165
+	# model configs
+	cfg_list = exp_smoothing_configs()
+	# grid search
+	scores = grid_search(data[:,0], cfg_list, n_test)
+	print('done')
+	# list top 3 configs
+	for cfg, error in scores[:3]:
+		print(cfg, error)
