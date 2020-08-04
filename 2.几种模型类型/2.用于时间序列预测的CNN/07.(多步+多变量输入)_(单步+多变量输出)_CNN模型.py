aee7769aa610557809c23a4d8717a359b3dd58bf
yangwohenmai <zongxianyang@outlook.com>
2020-08-04 10:18:36 +0800

    update

---
+++ 2.几种模型类型/2.用于时间序列预测的CNN/07.(多步+多变量输入)_(单步+多变量输出)_CNN模型.py
@@ -0,0 +1,55 @@
+# multivariate output 1d cnn example
+from numpy import array
+from numpy import hstack
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+
+# split a multivariate sequence into samples
+def split_sequences(sequences, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequences)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the dataset
+		if end_ix > len(sequences)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix, :]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+in_seq1 = array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+in_seq2 = array([15, 25, 35, 45, 55, 65, 75, 85, 95])
+out_seq = array([in_seq1[i]+in_seq2[i] for i in range(len(in_seq1))])
+# 重构数据结构：长度为9的一维数组重构成9行1列的二维数组，(9,)->(9,1)
+in_seq1 = in_seq1.reshape((len(in_seq1), 1))
+in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+out_seq = out_seq.reshape((len(out_seq), 1))
+# horizontally stack columns
+dataset = hstack((in_seq1, in_seq2, out_seq))
+# choose a number of time steps
+n_steps = 3
+# 输入数据形状(6,3,3),6组输入数据，每组3个时间步，每个时间步3个特征：
+X, y = split_sequences(dataset, n_steps)
+# the dataset knows the number of features, e.g. 2
+n_features = X.shape[2]
+# define model
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(n_steps, n_features)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(50, activation='relu'))
+model.add(Dense(n_features))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=3000, verbose=0)
+# demonstrate prediction
+x_input = array([[70,75,145], [80,85,165], [90,95,185]])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
