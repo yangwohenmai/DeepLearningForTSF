04b053061b40ba8633cd55ec8ae86cb9921bdf64
yangwohenmai <zongxianyang@outlook.com>
2020-08-11 17:48:12 +0800

    update

---
+++ 2.几种模型类型/3.用于时间序列预测的LSTM/01.(多步+单变量输入)_(单步+单变量输出)_监督学习数据.py
@@ -0,0 +1,28 @@
+# univariate data preparation
+from numpy import array
+
+# 构造一元监督学习型数据
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# 获取待预测数据的位置
+		end_ix = i + n_steps
+		# 如果待预测数据超过序列长度，构造完成
+		if end_ix >= len(sequence):
+			break
+		# 分别汇总 输入 和 输出 数据集
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 3
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# summarize the data
+for i in range(len(X)):
+	print(X[i], y[i])
+
