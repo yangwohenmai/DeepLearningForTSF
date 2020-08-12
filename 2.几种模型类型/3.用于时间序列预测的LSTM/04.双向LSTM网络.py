26fd8a699cf6fab077bad982f464b27440b5b258
yangwohenmai <zongxianyang@outlook.com>
2020-08-12 17:36:58 +0800

    update

---
+++ 2.几种模型类型/3.用于时间序列预测的LSTM/04.双向LSTM网络.py
@@ -0,0 +1,45 @@
+from numpy import array
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Bidirectional
+
+# 构造一元监督学习型数据
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# 获取待预测数据的位置
+		end_ix = i + n_steps
+		# 如果待预测数据超过序列长度，构造完成
+		if end_ix >= len(sequence):
+			break
+		# 分别汇总 输入 和 输出 数据集
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# 定义时间步长，即每组用几个数据作为输入，预测另一个数据
+n_steps = 3
+# 定义每步包含1个特征值
+n_features = 1
+# 调用上述split_sequences函数，数据集data(9,3)变成输入输出对：X(6,3),y(6,)
+X, y = split_sequence(raw_seq, n_steps)
+# 重新调整X的形状，将X(6,3)的输入转换成X(6,3,1)，表示6组数据，每组数据步长为3，每步有1个特征值
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+# 双向LSTM网络
+model = Sequential()
+# 定义输入的格式input_shape为(None,3,1),因此在fit()时，传入X(6,3,1),y(6,)，模型就会明白这是6组输入输出对
+# 用Bidirectional来定义一个双向网络
+model.add(Bidirectional(LSTM(50, activation='relu'), input_shape=(n_steps, n_features)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=200, verbose=0)
+# demonstrate prediction
+x_input = array([70, 80, 90])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
---
+++ 2.几种模型类型/3.用于时间序列预测的LSTM/05..py
@@ -0,0 +1,79 @@
+from numpy import array
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import TimeDistributed
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 4
+# X(5,4) y(5,)
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, subsequences, 时间步, 特征值]
+n_features = 1
+n_seq = 2
+n_steps = 2
+"""
+[[[[10]
+   [20]]
+  [[30]
+   [40]]]
+
+ [[[20]
+   [30]]
+  [[40]
+   [50]]]
+
+ [[[30]
+   [40]]
+  [[50]
+   [60]]]
+
+ [[[40]
+   [50]]
+  [[60]
+   [70]]]
+
+ [[[50]
+   [60]]
+  [[70]
+   [80]]]]
+"""
+# X(5,2,2,1)
+X = X.reshape((X.shape[0], n_seq, n_steps, n_features))
+print(X)
+# define model
+model = Sequential()
+# input_shape=(None, 2, 1)
+model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, n_steps, n_features)))
+model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+model.add(TimeDistributed(Flatten()))
+model.add(LSTM(50, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=500, verbose=0)
+# demonstrate prediction
+x_input = array([60, 70, 80, 90])
+x_input = x_input.reshape((1, n_seq, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
