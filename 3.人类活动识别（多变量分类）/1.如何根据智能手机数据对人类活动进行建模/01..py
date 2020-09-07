fc11980558503a23478216b2bd1c450c3ac2a322
yangwohenmai <zongxianyang@outlook.com>
2020-09-07 17:39:24 +0800

    update

---
+++ 3.人类活动识别（多变量分类）/1.如何根据智能手机数据对人类活动进行建模/01..py
@@ -0,0 +1,67 @@
+
+#from numpy import dstack
+#from pandas import read_csv
+# 
+## load a single file as a numpy array
+#def load_file(filepath):
+#	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+#	return dataframe.values
+# 
+## load a list of files, such as x, y, z data for a given variable
+#def load_group(filenames, prefix=''):
+#	loaded = list()
+#	for name in filenames:
+#		data = load_file(prefix + name)
+#		loaded.append(data)
+#	# stack group so that features are the 3rd dimension
+#	loaded = dstack(loaded)
+#	return loaded
+# 
+## load the total acc data
+#filenames = ['total_acc_x_train.txt', 'total_acc_y_train.txt', 'total_acc_z_train.txt']
+#total_acc = load_group(filenames, prefix='E:/MyGit/BigDataFile/HARDataset/train/Inertial Signals/')
+#print(total_acc.shape)
+
+
+# load dataset
+from numpy import dstack
+from pandas import read_csv
+
+# load a single file as a numpy array
+def load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+
+# load a list of files, such as x, y, z data for a given variable
+def load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+
+# load a dataset group, such as train or test
+def load_dataset(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = load_group(filenames, filepath)
+	# load class output
+	y = load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+
+# load all train
+trainX, trainy = load_dataset('train', 'E:/MyGit/BigDataFile/HARDataset/')
+print(trainX.shape, trainy.shape)
+# load all test
+testX, testy = load_dataset('test', 'E:/MyGit/BigDataFile/HARDataset/')
+print(testX.shape, testy.shape)
