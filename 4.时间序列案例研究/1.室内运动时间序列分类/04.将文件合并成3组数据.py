f58effe4303ce9bc63a50de22ea6639e39291d71
yangwohenmai <zongxianyang@outlook.com>
2020-09-17 18:06:30 +0800

    update

--- 4.时间序列案例研究/1.室内运动时间序列分类/04.将文件合并成3组数据.py
+++ 4.时间序列案例研究/1.室内运动时间序列分类/04.将文件合并成3组数据.py
@@ -4,14 +4,14 @@ from numpy import array
 from numpy import savetxt
 from pandas import read_csv
 
-# return list of traces, and arrays for targets, groups and paths
+# 加载IndoorMovement/dataset和IndoorMovement/groups下的所有文件
 def load_dataset(prefix=''):
 	grps_dir, data_dir = prefix+'groups/', prefix+'dataset/'
-	# load mapping files
+	# 读取单个文件
 	targets = read_csv(data_dir + 'MovementAAL_target.csv', header=0)
 	groups = read_csv(grps_dir + 'MovementAAL_DatasetGroup.csv', header=0)
 	paths = read_csv(grps_dir + 'MovementAAL_Paths.csv', header=0)
-	# load traces
+	# 加载IndoorMovement/dataset文件夹下除了以_target.csv结尾以外的文件
 	sequences = list()
 	for name in listdir(data_dir):
 		filename = data_dir + name
@@ -19,6 +19,7 @@ def load_dataset(prefix=''):
 			continue
 		df = read_csv(filename, header=0)
 		values = df.values
+		# 将每个文件里的数据加载到sequences中
 		sequences.append(values)
 	return sequences, targets.values[:,1], groups.values[:,1], paths.values[:,1]
 
@@ -26,32 +27,35 @@ def load_dataset(prefix=''):
 def create_dataset(sequences, targets):
 	# create the transformed dataset
 	transformed = list()
+	# 每行数据有4列
 	n_vars = 4
+	# 所有文件中最少的一个文件只有19行数据，所以每个文件只取最后19行数据，统一维度
 	n_steps = 19
 	# process each trace in turn
 	for i in range(len(sequences)):
 		seq = sequences[i]
 		vector = list()
-		# last n observations
+		# 从后向前取n_steps=19行数据，作为输入
 		for row in range(1, n_steps+1):
+			# 每行有n_vars=4列数据
 			for col in range(n_vars):
 				vector.append(seq[-row, col])
-		# add output
+		# 从targets文件中添加输出
 		vector.append(targets[i])
-		# store
+		# 将构建好的“输入->输出”对，加载到transformed中
 		transformed.append(vector)
-	# prepare array
+	# 将列表构建成数组
 	transformed = array(transformed)
 	transformed = transformed.astype('float32')
 	return transformed
 
-# load dataset
+# 加载数据
 sequences, targets, groups, paths = load_dataset("IndoorMovement/")
-# separate traces
+# 根据MovementAAL_DatasetGroup.csv文件中的序号，将sequences数据分成seq1、seq2、seq3三组
 seq1 = [sequences[i] for i in range(len(groups)) if groups[i]==1]
 seq2 = [sequences[i] for i in range(len(groups)) if groups[i]==2]
 seq3 = [sequences[i] for i in range(len(groups)) if groups[i]==3]
-# separate target
+# 根据MovementAAL_DatasetGroup.csv文件中的序号，将targets数据分成targets1、targets2、targets3三组
 targets1 = [targets[i] for i in range(len(groups)) if groups[i]==1]
 targets2 = [targets[i] for i in range(len(groups)) if groups[i]==2]
 targets3 = [targets[i] for i in range(len(groups)) if groups[i]==3]
