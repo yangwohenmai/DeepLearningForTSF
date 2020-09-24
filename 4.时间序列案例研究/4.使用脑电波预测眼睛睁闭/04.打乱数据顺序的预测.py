62120fcfa8c65cfb93f399ef032b99cf3e17fdd4
yangwohenmai <zongxianyang@outlook.com>
2020-09-24 17:49:47 +0800

    update

---
+++ 4.时间序列案例研究/4.使用脑电波预测眼睛睁闭/04.打乱数据顺序的预测.py
@@ -0,0 +1,21 @@
+# knn for predicting eye state
+from pandas import read_csv
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+# load the dataset
+data = read_csv('EEG_Eye_State_no_outliers.csv', header=None)
+values = data.values
+# split data into inputs and outputs
+X, y = values[:, :-1], values[:, -1]
+# split the dataset
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.1, shuffle=True, random_state=1)
+# define model
+model = KNeighborsClassifier(n_neighbors=3)
+# fit model on train set
+model.fit(trainX, trainy)
+# forecast test set
+yhat = model.predict(testX)
+# evaluate predictions
+score = accuracy_score(testy, yhat)
+print(score)
