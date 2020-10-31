90ea1f124033166fd798ac4c39af3773df540171
yangwohenmai <zongxianyang@outlook.com>
2020-10-31 22:41:43 +0800

    update

---
+++ 6.预测空气污染（多变量，多步骤）/2.空气污染预测的BaseLine模型/03.预测每一天中每小时的平均值.py
@@ -0,0 +1,139 @@
+# forecast global median by hour of day
+from numpy import loadtxt
+from numpy import nan
+from numpy import isnan
+from numpy import count_nonzero
+from numpy import unique
+from numpy import array
+from numpy import nanmedian
+from matplotlib import pyplot
+
+# split the dataset by 'chunkID', return a list of chunks
+def to_chunks(values, chunk_ix=0):
+	chunks = list()
+	# get the unique chunk ids
+	chunk_ids = unique(values[:, chunk_ix])
+	# group rows by chunk id
+	for chunk_id in chunk_ids:
+		selection = values[:, chunk_ix] == chunk_id
+		chunks.append(values[selection, :])
+	return chunks
+
+# return a list of relative forecast lead times
+def get_lead_times():
+	return [1, 2, 3, 4, 5, 10, 17, 24, 48, 72]
+
+# forecast all lead times for one variable
+def forecast_variable(train_chunks, chunk_train, chunk_test, lead_times, target_ix):
+	forecast = list()
+	# convert target number into column number
+	col_ix = 3 + target_ix
+	# enumerate lead times
+	for i in range(len(lead_times)):
+		# get the hour for this forecast lead time
+		hour = chunk_test[i, 2]
+		# check for no test data
+		if isnan(hour):
+			forecast.append(nan)
+			continue
+		# get all rows in training for this hour
+		all_rows = list()
+		for rows in train_chunks:
+			[all_rows.append(row) for row in rows[rows[:,2]==hour]]
+		# calculate the central tendency for target
+		all_rows = array(all_rows)
+		value = nanmedian(all_rows[:, col_ix])
+		forecast.append(value)
+	return forecast
+
+# forecast for each chunk, returns [chunk][variable][time]
+def forecast_chunks(train_chunks, test_input):
+	lead_times = get_lead_times()
+	predictions = list()
+	# enumerate chunks to forecast
+	for i in range(len(train_chunks)):
+		# enumerate targets for chunk
+		chunk_predictions = list()
+		for j in range(39):
+			yhat = forecast_variable(train_chunks, train_chunks[i], test_input[i], lead_times, j)
+			chunk_predictions.append(yhat)
+		chunk_predictions = array(chunk_predictions)
+		predictions.append(chunk_predictions)
+	return array(predictions)
+
+# convert the test dataset in chunks to [chunk][variable][time] format
+def prepare_test_forecasts(test_chunks):
+	predictions = list()
+	# enumerate chunks to forecast
+	for rows in test_chunks:
+		# enumerate targets for chunk
+		chunk_predictions = list()
+		for j in range(3, rows.shape[1]):
+			yhat = rows[:, j]
+			chunk_predictions.append(yhat)
+		chunk_predictions = array(chunk_predictions)
+		predictions.append(chunk_predictions)
+	return array(predictions)
+
+# calculate the error between an actual and predicted value
+def calculate_error(actual, predicted):
+	# give the full actual value if predicted is nan
+	if isnan(predicted):
+		return abs(actual)
+	# calculate abs difference
+	return abs(actual - predicted)
+
+# evaluate a forecast in the format [chunk][variable][time]
+def evaluate_forecasts(predictions, testset):
+	lead_times = get_lead_times()
+	total_mae, times_mae = 0.0, [0.0 for _ in range(len(lead_times))]
+	total_c, times_c = 0, [0 for _ in range(len(lead_times))]
+	# enumerate test chunks
+	for i in range(len(test_chunks)):
+		# convert to forecasts
+		actual = testset[i]
+		predicted = predictions[i]
+		# enumerate target variables
+		for j in range(predicted.shape[0]):
+			# enumerate lead times
+			for k in range(len(lead_times)):
+				# skip if actual in nan
+				if isnan(actual[j, k]):
+					continue
+				# calculate error
+				error = calculate_error(actual[j, k], predicted[j, k])
+				# update statistics
+				total_mae += error
+				times_mae[k] += error
+				total_c += 1
+				times_c[k] += 1
+	# normalize summed absolute errors
+	total_mae /= total_c
+	times_mae = [times_mae[i]/times_c[i] for i in range(len(times_mae))]
+	return total_mae, times_mae
+
+# summarize scores
+def summarize_error(name, total_mae, times_mae):
+	# print summary
+	lead_times = get_lead_times()
+	formatted = ['+%d %.3f' % (lead_times[i], times_mae[i]) for i in range(len(lead_times))]
+	s_scores = ', '.join(formatted)
+	print('%s: [%.3f MAE] %s' % (name, total_mae, s_scores))
+	# plot summary
+	pyplot.plot([str(x) for x in lead_times], times_mae, marker='.')
+	pyplot.show()
+
+# load dataset
+train = loadtxt(r'D:\咗MyGit\BigDataFile\dsg-hackathon\naive_train.csv', delimiter=',')
+test = loadtxt(r'D:\咗MyGit\BigDataFile\dsg-hackathon\naive_test.csv', delimiter=',')
+# group data by chunks
+train_chunks = to_chunks(train)
+test_chunks = to_chunks(test)
+# forecast
+test_input = [rows[:, :3] for rows in test_chunks]
+forecast = forecast_chunks(train_chunks, test_input)
+# evaluate forecast
+actual = prepare_test_forecasts(test_chunks)
+total_mae, times_mae = evaluate_forecasts(forecast, actual)
+# summarize forecast
+summarize_error('Global Median by Hour', total_mae, times_mae)
